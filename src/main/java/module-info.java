e31ddf3a09407c568efadda7f2a4711f7fbb6c35
guillaume <guillaume.przybysewski@etu.univ-nantes.fr>
2023-03-06 10:14:57 +0100

    yes

---
+++ src/main/java/module-info.java
@@ -0,0 +1,8 @@
+module com.example.footballito {
+    requires javafx.controls;
+    requires javafx.fxml;
+
+
+    opens com.example.footballito to javafx.fxml;
+    exports com.example.footballito;
+}
